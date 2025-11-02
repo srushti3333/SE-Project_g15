58b582eecf28809809c2f3fcbb7a22a1b8f10f8f
Srushti Thakar <srushtidthakar2808@gmail.com>
2025-11-02 15:22:02 -0500

    Revert "Integrated Flask Backend with React Frontend (PostgreSQL + JWT Auth)"

--- backend/routes/auth_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from controllers.auth_controller import register_user, login_user
-
-auth_bp = Blueprint('auth_bp', __name__)
-
-auth_bp.route('/register', methods=['POST'])(register_user)
-auth_bp.route('/login', methods=['POST'])(login_user)
